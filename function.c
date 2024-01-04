d9a1c50d4aee0e9d7a90c9e3c87754ae9aa9ec5b
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-04 16:59:10 -0300

    monty

--- function.c
+++ function.c
@@ -1,22 +1,22 @@
 #include "monty.h"
 void exe_push(char * number){
-    printf("exe_push\n");
+    printf("exe_push %s\n" number);
 }
 void exe_pall(char * number){
-     printf("exe_pall\n");
+     printf("exe_pall %s\n" number);
 }
 void exe_pint(char * number){
-    printf("exe_pint\n");
+    printf("exe_pint %s\n" number);
 }
 void exe_pop(char * number){
-    printf("exe_pop\n");
+    printf("exe_pop %s\n" number);
 }
 void exe_swap(char * number){
-    printf("exe_swap\n");
+    printf("exe_swap %s\n" number);
 }
 void exe_add(char * number){
-    printf("exe_add\n");
+    printf("exe_add %s\n" number);
 }
 void exe_nop(char * number){
-    printf("exe_nop\n");
+    printf("exe_nop %s\n" number);
 }
