59208aebd9319735411e25ef5b389ea6ba35dbd4
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-05 18:42:23 -0300

    monty

--- execom.c
+++ execom.c
@@ -1,33 +1,5 @@
 #include "monty.h"
 /**
- * execom - Execute a command based on the provided command and number.
- * @command: A pointer to a string representing the command to be executed.
- * @number: A pointer to a string containing additional information for the command.
- * Return: (void)
- */
-void execom(stack_t **head, char * command, char * number, int linecount)
-{     
-        int counter = 0;
-        optionscommand options[] = {{"push", exe_push}, {"pall", exe_pall},
-		{"pint", exe_pint}, {"pop", exe_pop}, {"swap", exe_swap},
-		{"add", exe_add}, {"nop", exe_nop}, {NULL, NULL}};
-           
-    for (counter = 0 ; counter < 7 ; counter++)
-    {
-        if (strcmp(command, options[counter].name) == 0)
-    	{
-		    options[counter].f(head, number, linecount);
-		    break;
-	    }
-        else
-        {   
-            if (counter == 6)
-                printf("L<%i>: unknown instruction <%s>", linecount, command);
-                exit(EXIT_FAILURE);
-        }
-    }
-}
-/**
  * filelines - Count the number of lines in a given file.
  * @file: Pointer to the FILE structure representing the open file.
  * Return: The number of lines in the file.
@@ -46,5 +18,33 @@ int filelines(FILE *file)
             }
         }
         return line_count;
+}
+/**
+ * execom - Execute a command based on the provided command and number.
+ * @command: A pointer to a string representing the command to be executed.
+ * @number: A pointer to a string containing additional information for the command.
+ * Return: (void)
+ */
+
+void execom(stack_t **head, char * command, char * number, int linecount)
+{     
+        int counter = 0;
+        optionscommand options[] = {{"push", exe_push}, {"pall", exe_pall},
+		{"pint", exe_pint}, {"pop", exe_pop}, {"swap", exe_swap},
+		{"add", exe_add}, {"nop", exe_nop}, {NULL, NULL}};
+           
+    for (counter = 0 ; counter < 7 ; counter++)
+    {
+        if (strcmp(command, options[counter].name) == 0)
+    	{
+		    options[counter].f(head, number, linecount);
+		    break;
+	    }
+        else
+        {   
+            if (counter == 6)
+                printf("L<%i>: unknown instruction <opcode>", linecount);
+        }
+    }
 }
 
