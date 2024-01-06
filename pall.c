343df20ea9a8270c343647976aaca2614839b584
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-05 21:24:50 -0300

    monty

--- pall.c
+++ pall.c
@@ -6,12 +6,13 @@
  * @linecount: Unused parameter (required by function signature).
  */
 void exe_pall(stack_t **head, char *number, int linecount, char * command, char ** lines)
-{
+{	
+	printf("PRINTING: ");
 	(void)command;
 	(void)linecount;
 	(void)number;
 	(void)lines;
-	
+
 	stack_t *current_node = *head;
 
 	if (*head == NULL)
